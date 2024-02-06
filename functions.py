685d7d60a3d6fdbc92e923face560b26a0f85773
ángel Alcantar <rayoalcantar@gmail.com>
2024-02-06 15:05:18 -0600

    Se areglaron cosas de la gui, como el orden y atajos de teclado

--- functions.py
+++ functions.py
@@ -12,30 +12,34 @@ class converterApp(wx.Frame):
         panel = wx.Panel(self)
         vbox = wx.BoxSizer(wx.VERTICAL)
 
+        # Primero el cuadro de edición para la ruta del archivo
         name_to_convert = wx.StaticText(panel, label="Escribe o pega la ruta del archivo a convertir (con formato)")
         vbox.Add(name_to_convert, flag=wx.LEFT | wx.TOP, border=10)
 
         self.fileName = wx.TextCtrl(panel, style=wx.TE_PROCESS_ENTER)
         vbox.Add(self.fileName, proportion=0, flag=wx.EXPAND | wx.ALL, border=10)
-        nameFile=wx.StaticText(panel, label="escribe el nombre del archivo convertido:")
+
+        # Luego el botón para añadir el archivo, con atajo de teclado
+        self.select_file = wx.Button(panel, label="&Abrir el explorador")
+        self.select_file.Bind(wx.EVT_BUTTON, self.file_select)
+        vbox.Add(self.select_file, flag=wx.ALIGN_CENTER | wx.TOP | wx.BOTTOM, border=10)
+
+        # Después el cuadro para el nombre del archivo que se creará
+        nameFile = wx.StaticText(panel, label="Escribe el nombre del archivo convertido:")
         vbox.Add(nameFile, flag=wx.LEFT | wx.TOP, border=10)
         self.name_from_file = wx.TextCtrl(panel, style=wx.TE_PROCESS_ENTER)
         vbox.Add(self.name_from_file, proportion=0, flag=wx.EXPAND | wx.ALL, border=10)
 
-        self.select_file = wx.Button(panel, label="Abrir el explorador")
-        self.select_file.Bind(wx.EVT_BUTTON, self.file_select)
-        vbox.Add(self.select_file, flag=wx.ALIGN_CENTER | wx.TOP | wx.BOTTOM, border=10)
-
-        self.ready = wx.Button(panel, label="Convertir")
+        # Finalmente, los botones restantes con sus atajos
+        self.ready = wx.Button(panel, label="&Convertir")
         self.ready.Bind(wx.EVT_BUTTON, self.audio_converter)
         vbox.Add(self.ready, flag=wx.ALIGN_CENTER | wx.TOP | wx.BOTTOM, border=10)
 
-        self.exit_button = wx.Button(panel, label="Salir del programa")
+        self.exit_button = wx.Button(panel, label="&Salir del programa")
         self.exit_button.Bind(wx.EVT_BUTTON, self.exit_program)
         vbox.Add(self.exit_button, flag=wx.ALIGN_CENTER | wx.TOP | wx.BOTTOM, border=10)
 
         panel.SetSizer(vbox)
-
     def file_select(self, event):
         with wx.FileDialog(self, "Seleccionar archivo de audio", wildcard="Archivos de audio (*.mp3;*.wav;*.opus;*.flac;*.m4a;*.ogg)|*.mp3;*.wav;*.opus;*.flac;*.m4a;*.ogg", style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST) as fileDialog:
             if fileDialog.ShowModal() == wx.ID_CANCEL:
